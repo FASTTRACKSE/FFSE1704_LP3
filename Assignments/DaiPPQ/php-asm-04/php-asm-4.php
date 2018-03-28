df50d13677a694f74e8762258b10f29a689dd786
QuangDaiPhanPham <ffse1704002@st.fasttrack.edu.vn>
2018-03-28 13:45:54 +0700

    update asm 4 5

--- Assignments/DaiPPQ/php-asm-04/php-asm-4.php
+++ Assignments/DaiPPQ/php-asm-04/php-asm-4.php
@@ -1,4 +1,5 @@
-<table border="1px" cellspacing="0px" width="50%">
+<form action="php-asm-4b.php" method="POST" enctype="multipart/form-data">
+	<table border="1px" cellspacing="0px" width="50%">
 	<tr>
 		<div>
 			<th colspan="2" style="text-align: left; background: #dfdfdf">Đăng ký tài khoản</th>
@@ -7,31 +8,31 @@
 	<tr>
 		<td>Tên đăng nhập (*)</td>
 		<td>
-			<input type="name" name="text">
+			<input type="text" name="username">
 		</td>
 	</tr>
 	<tr>
 		<td>Mật khẩu (*)</td>
 		<td>
-			<input type="name" name="text">
+			<input type="text" name="password">
 		</td>
 	</tr>
 	<tr>
 		<td>Họ và tên (*)</td>
 		<td>
-			<input type="name" name="text">
+			<input type="text" name="fullname">
 		</td>
 	</tr>
 	<tr>
 		<td>Ảnh đại diện (*)</td>
 		<td>
-			<input type="name" name="text">
+			<input type="file" name="avatar" value="Chọn tệp">
 		</td>
 	</tr>
 	<tr>
 		<td>Địa chỉ (*)</td>
 		<td>
-			<select name="diachi">
+			<select name="address">
 				<option>Hà Nội</option>
 				<option>Đà Nẵng</option>
 				<option>Hồ Chí Minh</option>
@@ -41,27 +42,25 @@
 	<tr>
 		<td>Giới tính</td>
 		<td>
-			<input type="radio" value="Male" name="gender">Nam
-			<br>
-			<input type="radio" value="Female" name="gender">Nữ
+			<input type="radio" name="gender" value="Nam" checked>Nam<br>
+			<input type="radio" name="gender" value="Nữ">Nữ
 		</td>
 	</tr>
 	<tr>
 		<td>Sở thích</td>
 		<td>
-			<input type="checkbox" value="Male" name="gender">Xem phim
-			<br>
-			<input type="checkbox" value="Female" name="gender">Nghe nhạc
-			<br>
-			<input type="checkbox" value="Female" name="gender">Đọc báo
+			<input type="checkbox" value="Xem phim" name="favorite[]" checked>Xem phim<br>
+			<input type="checkbox"  value="Nghe nhạc" name="favorite[]">Nghe nhạc<br>
+			<input type="checkbox" value="Đọc báo" name="favorite[]">Đọc báo
 		</td>
 	</tr>
 	<tr>
 		<td></td>
 		<td>
-			<input type="submit" value="Xác nhận">
-			<input type="submit" value="Hủy bỏ">
+			<input type="submit" name="submit" value="Xác nhận"> 
+			<input type="reset" name="cancel" value="Hủy bỏ">
 		</td>
 	</tr>
 	
 </table>
+</form>

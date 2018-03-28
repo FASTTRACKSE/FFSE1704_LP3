1c89aa1dd3e860d6c5801799606db40cc8462f7d
LeeKharHoongNhat <36950051+LeeKharHoongNhat@users.noreply.github.com>
2018-03-28 14:17:15 +0700

    .

--- Assignments/lamnt/session1.php
+++ Assignments/lamnt/session1.php
@@ -3,21 +3,24 @@
 
 	if(isset($_POST['submit'])){
 		$id= $_POST['id'];
-		$name= $_POST['ten'];
-		$age= $_POST['tuoi'];
+		$hoa= $_POST['hoa'];
+		$sl= $_POST['sl'];
+		$gia= $_POST['sl'];
 		if(isset($_SESSION['danhsach'][$id])){
-			$_SESSION['danhsach'][$id]['age']=$age;
+			$_SESSION['danhsach'][$id]['sl']=$sl;
 		}else{
 			$_SESSION['danhsach'][$id]=array(
-				'name'=>$name,
-				'age'=>$age
+				'hoa'=>$hoa,
+				'sl'=>$sl,
+				'gia'=>$gia
 			);
 		}
 	}
 ?>
 <form action='' method='POST'>
 	ID<input type='text' name='id'/>
-	Ten<input type='text' name='ten'/>
-	Tuoi<input type='text' name='tuoi'/>
+	hoa<input type='text' name='hoa'/>
+	sl<input type='text' name='sl'/>
+	gia<input type='text' name='gia'/>
 	<input type='submit' name='submit'/>
 </form>

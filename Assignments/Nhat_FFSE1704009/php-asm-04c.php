988cb91df1ca00d02c5caac6688c84aca200832a
LeeKharHoongNhat <36950051+LeeKharHoongNhat@users.noreply.github.com>
2018-03-29 14:36:28 +0700

    .

--- Assignments/Nhat_FFSE1704009/php-asm-04c.php
+++ Assignments/Nhat_FFSE1704009/php-asm-04c.php
@@ -5,3 +5,4 @@ $image = $_SESSION[$username]['avatar'];//lay ten file
 unlink($_SERVER['DOCUMENT_ROOT'].'/Nhat_FFSE1704009/images/'.$image);//xoa file
 unset($_SESSION[$_GET['username']]);//xoa phan tu session
 header("location: php-asm-04b.php");
+?>

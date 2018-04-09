558434c8fc2ea05618bd4a0974f48692cf7d1855
xuanky <ffse1701005@st.fasttrack.edu.vn>
2018-04-09 21:36:46 +0700

    update

---
+++ Assignments/XuanKy/ffse170105-asm-09/application/views/user/users.php
@@ -0,0 +1,108 @@
+<!-- /. NAV SIDE  --><div id="page-wrapper">
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Quản lý danh mục</h2>
+            </div>
+        </div>
+        <!-- /. ROW  -->
+        <hr />
+
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Advanced Tables -->
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <a href="add.html" class="btn btn-success btn-md" id="">
+                                        <i class="fa fa-plus"></i>&nbsp;Thêm
+                                    </a>
+                                </div>
+                                <div class="col-sm-6" style="text-align: right;">
+                                    <form method="post" action="">
+                                        <input type="submit" name="search" value="Tìm kiếm" class="btn btn-warning btn-sm" style="float:right" />
+                                        <input type="search" class="form-control input-sm" placeholder="Nhập họ tên" style="float:right; width: 300px;" />
+                                        <div style="clear:both"></div>
+                                    </form><br />
+                                </div>
+                            </div>
+
+                            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Tên tài khoản</th>
+                                        <th>Họ và tên</th>
+                                        <th>Hình ảnh</th>
+                                        <th width="160px">Chức năng</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                                                        <tr class="odd gradeX">
+                                        <td>1</td>
+                                        <td>ffse1701001</td>
+                                        <td>No name</td>
+                                        <td class="center">
+                                            <img src="<?=base_url("assets/img/")?>1.png" alt="" height="80px" width="100px" />
+                                        </td>
+                                        <td class="center">
+                                            <a href="" title="" class="btn btn-primary"><i class="fa fa-edit "></i> Sửa</a>
+                                            <a href="" title="" class="btn btn-danger"><i class="fa fa-pencil"></i> Xóa</a>
+                                        </td>
+                                    </tr>
+                                                                        <tr class="even gradeX">
+                                        <td>2</td>
+                                        <td>ffse1701002</td>
+                                        <td>No name</td>
+                                        <td class="center">
+                                            <img src="<?=base_url("assets/img/")?>1.png" alt="" height="80px" width="100px" />
+                                        </td>
+                                        <td class="center">
+                                            <a href="" title="" class="btn btn-primary"><i class="fa fa-edit "></i> Sửa</a>
+                                            <a href="" title="" class="btn btn-danger"><i class="fa fa-pencil"></i> Xóa</a>
+                                        </td>
+                                    </tr>
+                                                                        <tr class="odd gradeX">
+                                        <td>3</td>
+                                        <td>ffse1701003</td>
+                                        <td>No name</td>
+                                        <td class="center">
+                                            <img src="<?=base_url("assets/img/")?>1.png" alt="" height="80px" width="100px" />
+                                        </td>
+                                        <td class="center">
+                                            <a href="" title="" class="btn btn-primary"><i class="fa fa-edit "></i> Sửa</a>
+                                            <a href="" title="" class="btn btn-danger"><i class="fa fa-pencil"></i> Xóa</a>
+                                        </td>
+                                    </tr>
+                                                                    </tbody>
+                            </table>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="dataTables_info" id="dataTables-example_info" style="margin-top:27px">Hiển thị từ 1 đến 3 của 10 tài khoản</div>
+                                </div>
+                                <div class="col-sm-6" style="text-align: right;">
+                                    <div class="dataTables_paginate paging_simple_numbers" id="dataTables-example_paginate">
+                                        <ul class="pagination">
+                                            <li class="paginate_button previous disabled" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_previous"><a href="#">Trang trước</a></li>
+                                            <li class="paginate_button active" aria-controls="dataTables-example" tabindex="0"><a href="#">1</a></li>
+                                            <li class="paginate_button " aria-controls="dataTables-example" tabindex="0"><a href="#">2</a></li>
+                                            <li class="paginate_button " aria-controls="dataTables-example" tabindex="0"><a href="#">3</a></li>
+                                            <li class="paginate_button " aria-controls="dataTables-example" tabindex="0"><a href="#">4</a></li>
+                                            <li class="paginate_button " aria-controls="dataTables-example" tabindex="0"><a href="#">5</a></li>
+                                            <li class="paginate_button next" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_next"><a href="#">Trang tiếp</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <!--End Advanced Tables -->
+            </div>
+        </div>
+    </div>
+
+</div>
--- Assignments/XuanKy/xem.php
+++ Assignments/XuanKy/xem.php
@@ -11,7 +11,7 @@
 <?php
 require_once "sv.php";
 $ahihi= connectDB('localhost','xuanky','12345','ffse1704');
-$id=$_GET['id'];
+$id=$_GET['i'];
 $sql="SELECT * FROM lms_users WHERE user_id=$id";
 $rv=$ahihi->query($sql);
 $item=$rv->fetch_assoc();
